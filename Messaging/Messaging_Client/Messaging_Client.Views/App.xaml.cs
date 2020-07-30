203f195ecddd07d4c586c79990b2b01530a17df3
Shantanu.Methikar <smethikar18@gmail.com>
2020-07-30 23:01:12 +0530

    changed the namespace and added code in app.xaml.cs to launch MainWindow Styled for send button for mouse over and click

--- Messaging/Messaging_Client/Messaging_Client.Views/App.xaml.cs
+++ Messaging/Messaging_Client/Messaging_Client.Views/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace MessagingViews
+﻿namespace Messaging_Client.Views
 {
     using System.Windows;
 
@@ -7,5 +7,18 @@
     /// </summary>
     public partial class App : Application
     {
+        #region Protected Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ViewModels.BaseViewModel mainVM = new ViewModels.MainViewModel();
+
+            MainWindow mainWin = new MainWindow();
+            mainWin.DataContext = mainVM;
+
+            mainWin.Show();
+        }
+
+        #endregion Protected Methods
     }
 }
