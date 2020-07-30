d55c345c8c97f659a0f3ffbfcc13af19d00f7f59
Shantanu.Methikar <smethikar18@gmail.com>
2020-07-30 22:59:14 +0530

    Test added to check if all the classes in ViewModel namespace are derived from BaseVM class

--- Messaging/Messaging_Client/Messaging_Client.UnitTest/FrontEndUnitTests.cs
+++ Messaging/Messaging_Client/Messaging_Client.UnitTest/FrontEndUnitTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace Messaging_Client.UnitTest
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using System.Reflection;
 
     [TestClass]
     public class FrontEndUnitTests
@@ -8,10 +11,32 @@
         #region Public Methods
 
         [TestMethod]
-        public void TestMethod1()
+        public void AllVMs_DerivedFromBaseVM()
         {
+            Assembly vmAssembly = typeof(ViewModels.BaseViewModel).GetTypeInfo().Assembly;
+            Type[] typelist = GetTypesInNamespace(vmAssembly, "Messaging_Client.ViewModels");
+
+            foreach (var type in typelist)
+            {
+                if (type != typeof(ViewModels.BaseViewModel))
+                {
+                    Assert.AreEqual(typeof(ViewModels.BaseViewModel), type.BaseType);
+                }
+            }
         }
 
         #endregion Public Methods
+
+        #region Private Methods
+
+        private Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+        {
+            return
+              assembly.GetTypes()
+                      .Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                      .ToArray();
+        }
+
+        #endregion Private Methods
     }
 }
