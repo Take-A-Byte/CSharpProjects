cc4d6697ecb3b4b30e0b16851fbc6aec5016e739
Shantanu.Methikar <smethikar18@gmail.com>
2020-08-01 22:45:40 +0530

    added server project

---
+++ Messaging/Messaging_Server/Messaging_Server/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Messaging_Server
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
