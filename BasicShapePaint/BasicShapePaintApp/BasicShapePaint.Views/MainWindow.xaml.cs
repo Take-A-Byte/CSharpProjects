329462b014c277ceebcd5b14d79ab7410918f434
Shantanu.Methikar <smethikar18@gmail.com>
2020-11-10 12:33:59 +0530

    created project structure

---
+++ BasicShapePaint/BasicShapePaintApp/BasicShapePaint.Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicShapePaint.Views
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Public Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
