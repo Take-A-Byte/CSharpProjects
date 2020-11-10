bf2b96ac47cb7d15e428e9c89ecb90539f47b2b3
Shantanu.Methikar <smethikar18@gmail.com>
2020-11-10 15:48:35 +0530

    Basic UI ready

--- BasicShapePaint/BasicShapePaintApp/BasicShapePaint.Views/MainWindow.xaml.cs
+++ BasicShapePaint/BasicShapePaintApp/BasicShapePaint.Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿namespace BasicShapePaint.Views
 {
     using System.Windows;
+    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -15,5 +16,22 @@
         }
 
         #endregion Public Constructors
+
+        #region Private Methods
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
+            colorDialog.AllowFullOpen = true;
+            colorDialog.FullOpen = true;
+            colorDialog.SolidColorOnly = true;
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorPicker.Background = new SolidColorBrush(Color.FromRgb(
+                    colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B));
+            }
+        }
+
+        #endregion Private Methods
     }
 }
